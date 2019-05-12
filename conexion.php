37a92dac16fcef3c75152f4b6fbfb514e385066d
Carlos Serrano <carlosclowm@gmail.com>
2019-05-12 04:31:04 -0500

    Git Ignore Agregado

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php 
+//Se elimino con gitignore este archivo
 	$conexion = new mysqli("ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "jf7vvceknx5bpl8m", "qnaf8zl7922ow5dx", "ayb84n79a8baw4t8");
 
 	if($conexion->connect_errno){
